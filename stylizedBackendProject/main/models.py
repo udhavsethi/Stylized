75c2d868e588e606eefdecf6694b06e5ed9f762a
udhavsethi <es12b1022@iith.ac.in>
2015-12-19 17:36:12 +0530

    Added models for the main app

--- stylizedBackendProject/main/models.py
+++ stylizedBackendProject/main/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	cat_name = models.CharField(max_length=200)
+
+class Style(models.Model):
+	category = models.ForeignKey(Category)
+	style_name = models.CharField(max_length=200)
+	suitable_for = models.CharField(max_length=20)		#men/women/children
+	description = models.TextField()
+	views = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.style_name
+
+class Salon(models.Model):
+	salon_name = models.CharField(max_length=200)
+	salon_location = models.CharField(max_length=200)
+	phone_num = models.CharField(max_length=200)		#can store multiple phone numbers
+	salon_email = models.EmailField()
+	address = models.TextField()
+	gmaps_url = models.URLField()
+	avg_rating = models.CharField(max_length=20)
+	style_menu = models.ManyToManyField(Style, through='StyleSalon')			#styles provided by the salon
+
+	def __str__(self):
+		return self.salon_name
+
+class StyleSalon(models.Model):
+	style = models.ForeignKey(Style)
+	salon = models.ForeignKey(Salon)
+	ss_rating = models.CharField(max_length=20)			#style-salon rating
+
+class User(models.Model):
+	firstname = models.CharField(max_length=200)
+	lastname = models.CharField(max_length=200)
+	user_email = models.EmailField()
+	password = models.CharField(max_length=200)
+	gender = models.CharField(max_length=20)
+	age = models.IntegerField(default=0)
+	user_location = models.CharField(max_length=200)
+	login_via = models.ChharField(max_length=20)		#fb/insta/gmail
+	history = models.ManyToManyField(StyleSalon, through='UserStyleSalon')		#All the style-salon pairs tried by the user
+
+	def __str__(self):
+		return self.user_email
+
+class UserStyleSalon(models.model):
+	user = models.ForeignKey(User)
+	style_salon = models.ForeignKey(StyleSalon)
+	user_rating = models.CharField(max_length=20)		#user-salon-style rating
+
+class Transaction(models.Model):
+	user = models.ForeignKey(User)
+	style_salon = models.ForeignKey(StyleSalon)
